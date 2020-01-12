70b8b7a03c0e8759246d69bf518aff7edd1e8bd7
Thomas Jeffery <thomasjeffery3222@hotmail.com>
2020-01-12 13:13:42 -0300

    aa

--- capavista/inicio.Designer.cs
+++ capavista/inicio.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             this.panelLateral = new System.Windows.Forms.Panel();
-            this.panelDeposito = new System.Windows.Forms.Panel();
             this.subpanelDeposito = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panelDeposito = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
             this.subpanelprod = new System.Windows.Forms.Panel();
+            this.btnBuscarProd = new System.Windows.Forms.Button();
+            this.btnNuevoProd = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panelproducto = new System.Windows.Forms.Panel();
+            this.btnProd = new System.Windows.Forms.Button();
             this.panelLogo = new System.Windows.Forms.Panel();
             this.panel9 = new System.Windows.Forms.Panel();
-            this.btnProd = new System.Windows.Forms.Button();
-            this.btnNuevoProd = new System.Windows.Forms.Button();
-            this.btnBuscarProd = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.panelLateral.SuspendLayout();
-            this.panelDeposito.SuspendLayout();
             this.subpanelDeposito.SuspendLayout();
+            this.panelDeposito.SuspendLayout();
             this.subpanelprod.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panelproducto.SuspendLayout();
@@ -64,15 +64,6 @@
             this.panelLateral.Size = new System.Drawing.Size(200, 538);
             this.panelLateral.TabIndex = 0;
             // 
-            // panelDeposito
-            // 
-            this.panelDeposito.Controls.Add(this.button4);
-            this.panelDeposito.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelDeposito.Location = new System.Drawing.Point(0, 219);
-            this.panelDeposito.Name = "panelDeposito";
-            this.panelDeposito.Size = new System.Drawing.Size(200, 44);
-            this.panelDeposito.TabIndex = 2;
-            // 
             // subpanelDeposito
             // 
             this.subpanelDeposito.Controls.Add(this.button6);
@@ -83,6 +74,45 @@
             this.subpanelDeposito.Size = new System.Drawing.Size(200, 75);
             this.subpanelDeposito.TabIndex = 0;
             // 
+            // button6
+            // 
+            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button6.Location = new System.Drawing.Point(0, 31);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(200, 33);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 31);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panelDeposito
+            // 
+            this.panelDeposito.Controls.Add(this.button4);
+            this.panelDeposito.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelDeposito.Location = new System.Drawing.Point(0, 219);
+            this.panelDeposito.Name = "panelDeposito";
+            this.panelDeposito.Size = new System.Drawing.Size(200, 44);
+            this.panelDeposito.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 44);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Ventas";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // subpanelprod
             // 
             this.subpanelprod.Controls.Add(this.btnBuscarProd);
@@ -92,6 +122,28 @@
             this.subpanelprod.Name = "subpanelprod";
             this.subpanelprod.Size = new System.Drawing.Size(200, 71);
             this.subpanelprod.TabIndex = 1;
+            // 
+            // btnBuscarProd
+            // 
+            this.btnBuscarProd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBuscarProd.Location = new System.Drawing.Point(0, 32);
+            this.btnBuscarProd.Name = "btnBuscarProd";
+            this.btnBuscarProd.Size = new System.Drawing.Size(200, 32);
+            this.btnBuscarProd.TabIndex = 1;
+            this.btnBuscarProd.Text = "Buscar";
+            this.btnBuscarProd.UseVisualStyleBackColor = true;
+            this.btnBuscarProd.Click += new System.EventHandler(this.BtnBuscarProd_Click);
+            // 
+            // btnNuevoProd
+            // 
+            this.btnNuevoProd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNuevoProd.Location = new System.Drawing.Point(0, 0);
+            this.btnNuevoProd.Name = "btnNuevoProd";
+            this.btnNuevoProd.Size = new System.Drawing.Size(200, 32);
+            this.btnNuevoProd.TabIndex = 0;
+            this.btnNuevoProd.Text = "Nuevo";
+            this.btnNuevoProd.UseVisualStyleBackColor = true;
+            this.btnNuevoProd.Click += new System.EventHandler(this.BtnNuevoProd_Click);
             // 
             // panel2
             // 
@@ -120,6 +172,16 @@
             this.panelproducto.Size = new System.Drawing.Size(200, 42);
             this.panelproducto.TabIndex = 2;
             // 
+            // btnProd
+            // 
+            this.btnProd.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnProd.Location = new System.Drawing.Point(0, 0);
+            this.btnProd.Name = "btnProd";
+            this.btnProd.Size = new System.Drawing.Size(200, 42);
+            this.btnProd.TabIndex = 0;
+            this.btnProd.Text = "Productos";
+            this.btnProd.UseVisualStyleBackColor = true;
+            // 
             // panelLogo
             // 
             this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
@@ -136,68 +198,6 @@
             this.panel9.Size = new System.Drawing.Size(848, 538);
             this.panel9.TabIndex = 1;
             // 
-            // btnProd
-            // 
-            this.btnProd.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnProd.Location = new System.Drawing.Point(0, 0);
-            this.btnProd.Name = "btnProd";
-            this.btnProd.Size = new System.Drawing.Size(200, 42);
-            this.btnProd.TabIndex = 0;
-            this.btnProd.Text = "Productos";
-            this.btnProd.UseVisualStyleBackColor = true;
-            // 
-            // btnNuevoProd
-            // 
-            this.btnNuevoProd.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnNuevoProd.Location = new System.Drawing.Point(0, 0);
-            this.btnNuevoProd.Name = "btnNuevoProd";
-            this.btnNuevoProd.Size = new System.Drawing.Size(200, 32);
-            this.btnNuevoProd.TabIndex = 0;
-            this.btnNuevoProd.Text = "Nuevo";
-            this.btnNuevoProd.UseVisualStyleBackColor = true;
-            this.btnNuevoProd.Click += new System.EventHandler(this.BtnNuevoProd_Click);
-            // 
-            // btnBuscarProd
-            // 
-            this.btnBuscarProd.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btnBuscarProd.Location = new System.Drawing.Point(0, 32);
-            this.btnBuscarProd.Name = "btnBuscarProd";
-            this.btnBuscarProd.Size = new System.Drawing.Size(200, 32);
-            this.btnBuscarProd.TabIndex = 1;
-            this.btnBuscarProd.Text = "Buscar";
-            this.btnBuscarProd.UseVisualStyleBackColor = true;
-            this.btnBuscarProd.Click += new System.EventHandler(this.BtnBuscarProd_Click);
-            // 
-            // button4
-            // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button4.Location = new System.Drawing.Point(0, 0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(200, 44);
-            this.button4.TabIndex = 1;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.Location = new System.Drawing.Point(0, 0);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(200, 31);
-            this.button5.TabIndex = 1;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button6.Location = new System.Drawing.Point(0, 31);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(200, 33);
-            this.button6.TabIndex = 2;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
             // inicio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -208,8 +208,8 @@
             this.Name = "inicio";
             this.Text = "inicio";
             this.panelLateral.ResumeLayout(false);
-            this.panelDeposito.ResumeLayout(false);
             this.subpanelDeposito.ResumeLayout(false);
+            this.panelDeposito.ResumeLayout(false);
             this.subpanelprod.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panelproducto.ResumeLayout(false);
