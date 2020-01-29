dac41533bf8a60ca343dfade74605f0694d2e3b0
NeoCast <thomasjeffery3222@hotmail.com>
2020-01-29 16:46:59 -0300

    se realiza la venta, con falta de verificacion final

--- capavista/ventas.Designer.cs
+++ capavista/ventas.Designer.cs
@@ -31,24 +31,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventas));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.gridDetalles = new System.Windows.Forms.DataGridView();
-            this.btnbuscar = new System.Windows.Forms.PictureBox();
-            this.txbBuscar = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnVender = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.ventaTot = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.codProd = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.tProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.valorTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnbuscar = new System.Windows.Forms.PictureBox();
+            this.txbBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnVender = new System.Windows.Forms.Button();
+            this.ventaTot = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridDetalles)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnbuscar)).BeginInit();
@@ -64,7 +59,6 @@
             this.dataGridView1.Size = new System.Drawing.Size(508, 163);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellDoubleClick);
             this.dataGridView1.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_CellMouseDoubleClick);
             // 
             // gridDetalles
@@ -83,6 +77,41 @@
             this.gridDetalles.Name = "gridDetalles";
             this.gridDetalles.Size = new System.Drawing.Size(437, 420);
             this.gridDetalles.TabIndex = 1;
+            // 
+            // codProd
+            // 
+            this.codProd.HeaderText = "Codigo";
+            this.codProd.Name = "codProd";
+            this.codProd.ReadOnly = true;
+            // 
+            // tProducto
+            // 
+            this.tProducto.HeaderText = "T-Producto";
+            this.tProducto.Name = "tProducto";
+            this.tProducto.ReadOnly = true;
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "Descripcion";
+            this.descripcion.Name = "descripcion";
+            this.descripcion.ReadOnly = true;
+            // 
+            // cantidad
+            // 
+            this.cantidad.HeaderText = "Cantidad";
+            this.cantidad.Name = "cantidad";
+            this.cantidad.ReadOnly = true;
+            // 
+            // valor
+            // 
+            this.valor.HeaderText = "Valor";
+            this.valor.Name = "valor";
+            this.valor.ReadOnly = true;
+            // 
+            // valorTotal
+            // 
+            this.valorTotal.HeaderText = "Valor Total";
+            this.valorTotal.Name = "valorTotal";
             // 
             // btnbuscar
             // 
@@ -131,7 +160,7 @@
             this.btnVender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnVender.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnVender.ForeColor = System.Drawing.Color.White;
-            this.btnVender.Location = new System.Drawing.Point(799, 456);
+            this.btnVender.Location = new System.Drawing.Point(716, 514);
             this.btnVender.Name = "btnVender";
             this.btnVender.Size = new System.Drawing.Size(258, 65);
             this.btnVender.TabIndex = 30;
@@ -139,71 +168,12 @@
             this.btnVender.UseVisualStyleBackColor = false;
             this.btnVender.Click += new System.EventHandler(this.BtnGuardar_Click);
             // 
-            // label4
-            // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(44, 394);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(151, 20);
-            this.label4.TabIndex = 33;
-            this.label4.Text = "El cliente pagó con:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Location = new System.Drawing.Point(201, 396);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(101, 20);
-            this.textBox1.TabIndex = 34;
-            // 
-            // label5
-            // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(44, 456);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(110, 20);
-            this.label5.TabIndex = 35;
-            this.label5.Text = "Su vuelto es...";
-            // 
-            // label6
-            // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label6.Location = new System.Drawing.Point(172, 478);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(102, 37);
-            this.label6.TabIndex = 36;
-            this.label6.Text = "label6";
-            // 
-            // button1
-            // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.AutoSize = true;
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(50)))), ((int)(((byte)(70)))));
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(298, 478);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(258, 65);
-            this.button1.TabIndex = 37;
-            this.button1.Text = "Calcular";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // ventaTot
             // 
             this.ventaTot.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.ventaTot.AutoSize = true;
             this.ventaTot.Font = new System.Drawing.Font("Century Schoolbook", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ventaTot.Location = new System.Drawing.Point(805, 569);
+            this.ventaTot.Location = new System.Drawing.Point(817, 463);
             this.ventaTot.Name = "ventaTot";
             this.ventaTot.Size = new System.Drawing.Size(70, 20);
             this.ventaTot.TabIndex = 31;
@@ -216,46 +186,11 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label3.Location = new System.Drawing.Point(943, 556);
+            this.label3.Location = new System.Drawing.Point(955, 450);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(102, 37);
             this.label3.TabIndex = 32;
             this.label3.Text = "label3";
-            // 
-            // codProd
-            // 
-            this.codProd.HeaderText = "Codigo";
-            this.codProd.Name = "codProd";
-            this.codProd.ReadOnly = true;
-            // 
-            // tProducto
-            // 
-            this.tProducto.HeaderText = "T-Producto";
-            this.tProducto.Name = "tProducto";
-            this.tProducto.ReadOnly = true;
-            // 
-            // descripcion
-            // 
-            this.descripcion.HeaderText = "Descripcion";
-            this.descripcion.Name = "descripcion";
-            this.descripcion.ReadOnly = true;
-            // 
-            // cantidad
-            // 
-            this.cantidad.HeaderText = "Cantidad";
-            this.cantidad.Name = "cantidad";
-            this.cantidad.ReadOnly = true;
-            // 
-            // valor
-            // 
-            this.valor.HeaderText = "Valor";
-            this.valor.Name = "valor";
-            this.valor.ReadOnly = true;
-            // 
-            // valorTotal
-            // 
-            this.valorTotal.HeaderText = "Valor Total";
-            this.valorTotal.Name = "valorTotal";
             // 
             // ventas
             // 
@@ -263,11 +198,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
             this.ClientSize = new System.Drawing.Size(1080, 612);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.ventaTot);
             this.Controls.Add(this.btnVender);
@@ -298,11 +228,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnVender;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label ventaTot;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridViewTextBoxColumn codProd;
--- capavista/ventas.cs
+++ capavista/ventas.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using capalnegocio;
 
+
 namespace capavista
 {
     public partial class ventas : Form
@@ -30,99 +31,116 @@ namespace capavista
 
         private void BtnGuardar_Click(object sender, EventArgs e)
         {
-         
+            if (label3.Text != "")
+            {
+                double total = Convert.ToDouble(label3.Text);
 
                 int cantfila = dataGridView1.Rows.Count;
-                foreach (DataGridViewRow fila in gridDetalles.Rows)
+                if (cantfila > 0)
                 {
+                    ventaLN.LNnuevaVenta(total, System.DateTime.Now);
 
-
-
-                    if (fila != null)
+                    foreach (DataGridViewRow fila in gridDetalles.Rows)
                     {
+
+                        if (fila != null)
+                        {
                             if (fila.Index < cantfila - 1)
                             {
                                 int codProd = Convert.ToInt32(fila.Cells[0].Value.ToString());
-                                string descripcion = Convert.ToString(fila.Cells[1].Value.ToString());
-                                
-                                int cantidad = Convert.ToInt32(fila.Cells[4].Value.ToString());
+                                string tipoProd = Convert.ToString(fila.Cells[1].Value.ToString());
+                                string descripcion = Convert.ToString(fila.Cells[2].Value.ToString());
+                                int cantidad = Convert.ToInt32(fila.Cells[3].Value.ToString());
+                                double precio = Convert.ToDouble(fila.Cells[4].Value.ToString());
+                                double totalProd = Convert.ToDouble(fila.Cells[5].Value.ToString());
 
-                                //ventaLN.LNnuevaVenta();
 
                                 //ventaLN.LNdetalleVenta(codProd, cantidad, descripcion, );
-                    
+
                             }
-                             else
-                                {
-                                    MessageBox.Show("la fila es nula");
+                        }
+                        else
+                        {
+                            MessageBox.Show("la fila es nula");
+                        }
+                        //else if (fila = -1)
+                        //{
 
-                                }
+                        //}
 
+                        //    }
                     }
-                    //else if (fila = -1)
-                    //{
 
-                    //}
-
-                    //    }
-
-                   
 
                 }
 
-            MessageBox.Show("La carga se realizo con exito");
+                MessageBox.Show("La carga se realizo con exito");
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese algun producto antes de realizar la venta");
+            }
+
 
 
 
         }
          
-        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-           
-
-       
-        }
+ 
 
         private void Ventas_Load(object sender, EventArgs e)
         {
+
             dataGridView1.DataSource = productoLN.mostrarTodos();
+
+
         }
 
         private void DataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
-            foreach (DataGridViewRow fila in dataGridView1.SelectedRows)
-            {
 
-                // Creamos un array con los valores que vamos a insertar
-                // en el segundo control DataGridView.
-                //
-
-                object[] values = {
-
-                                          fila.Cells["codProd"].Value,
-                                          fila.Cells["tProducto"].Value,
-                                          fila.Cells["descripcion"].Value,
-                                          fila.Cells["cantidad"].Value,
-                                          fila.Cells["valor"].Value,
-                                          fila.Cells["valorTotal"].Value
+            DataGridViewRow fila = new DataGridViewRow();
+            DataGridViewRow fila2 = new DataGridViewRow();
+            int cantidad;
+            double precio, totalProd;
 
 
-                                  };
+           
 
-                // Creamos un nuevo objeto DataGridViewRow.
-                //
-                DataGridViewRow row = new DataGridViewRow();
+                fila = dataGridView1.SelectedRows[e.RowIndex];
+            cantidad = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("ingrese la cantidad: ").ToString());
+       
+                try
+                {
+               
+                precio = Convert.ToDouble(fila.Cells);
+                totalProd = precio * cantidad;
+                    fila2.CreateCells(gridDetalles);
+                    fila2.Cells[0].Value = fila.Cells[0].Value;
+                    fila2.Cells[1].Value = fila.Cells[1].Value;
+                    fila2.Cells[2].Value = fila.Cells[2].Value;
+                    fila2.Cells[3].Value = cantidad;
+                    fila2.Cells[4].Value = fila.Cells[3].Value;
+                    fila2.Cells[5].Value = totalProd;
+                    
+                gridDetalles.Rows.Add(fila);
 
-                // Creamos las celdas y las rellenamos con los valores existentes
-                // en el array.
-                //
-                row.CreateCells(gridDetalles, values);
+                label3.Text = Convert.ToString(Convert.ToDouble(label3.Text) + totalProd);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("selecciona una fila para realizar ésta accion");
+                    throw ex;
+                }
 
-                // Añadimos la nueva fila al segundo control DataGridView.
-                //
-                gridDetalles.Rows.Add(row);
+            
 
-            }
+
+
+
         }
+
     }
+    
 }
