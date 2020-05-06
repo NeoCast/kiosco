10aab89ff22ad726964f01e4428b24057b6f3989
Ivan Martinez <ivan.martinez782@gmail.com>
2020-05-06 18:41:21 -0300

    agrege el login devuelta como inicio

--- capavista/Program.cs
+++ capavista/Program.cs
@@ -13,7 +13,7 @@ namespace capavista
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Bienvenida());
+            Application.Run(new Login());
         }
     }
 }
